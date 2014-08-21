6649c75913ba853515fefc5324f87937068f9915
David Gomes <davidrafagomes@gmail.com>
2014-08-21 20:51:26 +0100

    Cleans up main.js.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* Set up localStorage */
+/* Set up localStorage for the high score */
 var highScore = 0;
 var currentScore = 0;
 
@@ -9,7 +9,6 @@ if (!localStorage["highScore"]) {
   highScore = 0;
 } else {
   highScore = parseInt(localStorage["highScore"]);
-  //console.log("High score:" + localStorage["highScore"]);
 }
 
 /* Populate array audios */
@@ -18,28 +17,12 @@ var audio_paths = ["assets/sfx/metal_on_metal",
                    "assets/sfx/brick_on_brick",
                    "assets/sfx/wood_on_wood"];
 
-for (var i = 0; i < audio_paths.length; i++) {
-  /*var audio = new Audio();
-  var canPlayOgg = !!audio.canPlayType && audio.canPlayType('audio/ogg; codecs="vorbis"') != "";
-  var path = audio_paths[i];
-
-  if (canPlayOgg) {
-    path = path + ".ogg";
-  } else {
-    path = path + ".mp3";
-  }
-
-  audio = new Audio(path);
-  audio.load();
-  audios.push(audio);*/
-}
-
 /* Disable finger swipe scrolling on mobile devices */
 document.ontouchmove = function(e) {
   e.preventDefault();
 };
 
-/* Start the title screen */
+/* Start Pentagine with the title screen state */
 penta.setup({ width: document.documentElement.clientWidth,
               height: document.documentElement.clientHeight,
               desiredFPS: 60,
